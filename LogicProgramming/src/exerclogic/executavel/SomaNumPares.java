b8fca1c3d2c9b18b470758bf971030b4afa7fc13
Gustavo de Azevedo <gudeazevedo@outlook.com>
2025-09-25 18:08:55 -0300

    criação do projeto

---
+++ LogicProgramming/src/exerclogic/executavel/SomaNumPares.java
@@ -0,0 +1,8 @@
+package exerclogic.executavel;
+
+public class SomaNumPares {
+	
+	/* Escreva um programa que receba um número inteiro N e calcule a soma de todos os números pares de 1 a N.
+	   Exemplo: Se N = 10, a soma deve ser 2 + 4 + 6 + 8 + 10 = 30.*/
+		int num;
+}
